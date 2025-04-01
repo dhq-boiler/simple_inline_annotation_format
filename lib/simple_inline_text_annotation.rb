9240efe112d4df3d5093ecd85de3d750eb865f92
xaiBUh29wX <arino.tamada@luxiar.com>
2025-04-01 16:44:54 +0900

    Initialize gem structure for simple_inline_text_annotation

---
+++ lib/simple_inline_text_annotation.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "simple_inline_text_annotation/version"
+
+module SimpleInlineTextAnnotation
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/simple_inline_text_annotation_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe SimpleInlineTextAnnotation do
+  it "has a version number" do
+    expect(SimpleInlineTextAnnotation::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
