eaed63f1118681de7fe0fbfe445b483124d92ff0
xaiBUh29wX <arino.tamada@luxiar.com>
2025-04-03 10:29:12 +0900

    Apply RuboCop auto-corrections to improve code style

--- spec/simple_inline_text_annotation/parser_spec.rb
+++ spec/simple_inline_text_annotation/parser_spec.rb
@@ -1,25 +1,29 @@
-require 'spec_helper'
+# frozen_string_literal: true
+
+require "spec_helper"
 
 RSpec.describe SimpleInlineTextAnnotation::Parser, type: :model do
-  describe '#parse' do
+  describe "#parse" do
     subject { SimpleInlineTextAnnotation.parse(source) }
 
-    context 'when source has annotation structure' do
-      let(:source) { '[Elon Musk][Person] is a member of the [PayPal Mafia][Organization].' }
-      let(:expected_format) { {
-        "text": "Elon Musk is a member of the PayPal Mafia.",
-        "denotations":[
-            {"span":{"begin": 0, "end": 9}, "obj":"Person"},
-            {"span":{"begin": 29, "end": 41}, "obj":"Organization"},
+    context "when source has annotation structure" do
+      let(:source) { "[Elon Musk][Person] is a member of the [PayPal Mafia][Organization]." }
+      let(:expected_format) do
+        {
+          "text": "Elon Musk is a member of the PayPal Mafia.",
+          "denotations": [
+            { "span": { "begin": 0, "end": 9 }, "obj": "Person" },
+            { "span": { "begin": 29, "end": 41 }, "obj": "Organization" }
           ]
-      } }
+        }
+      end
 
-      it 'parse as denotation' do
+      it "parse as denotation" do
         is_expected.to eq(expected_format)
       end
     end
 
-    context 'when surce has reference structure' do
+    context "when surce has reference structure" do
       let(:source) do
         <<~MD2
           [Elon Musk][Person] is a member of the [PayPal Mafia][Organization].
@@ -28,85 +32,93 @@ RSpec.describe SimpleInlineTextAnnotation::Parser, type: :model do
           [Organization]: https://example.com/Organization
         MD2
       end
-      let(:expected_format) { {
-        "text": "Elon Musk is a member of the PayPal Mafia.",
-        "denotations":[
-            {"span":{"begin": 0, "end": 9}, "obj":"https://example.com/Person"},
-            {"span":{"begin": 29, "end": 41}, "obj":"https://example.com/Organization"},
+      let(:expected_format) do
+        {
+          "text": "Elon Musk is a member of the PayPal Mafia.",
+          "denotations": [
+            { "span": { "begin": 0, "end": 9 }, "obj": "https://example.com/Person" },
+            { "span": { "begin": 29, "end": 41 }, "obj": "https://example.com/Organization" }
           ],
-        "config": {
-          "entity types": [
-            { "id": "https://example.com/Person", "label": "Person" },
-            { "id": "https://example.com/Organization", "label": "Organization" }
-          ]
+          "config": {
+            "entity types": [
+              { "id": "https://example.com/Person", "label": "Person" },
+              { "id": "https://example.com/Organization", "label": "Organization" }
+            ]
+          }
         }
-      } }
+      end
 
-      it 'parse as entity types and apply id to denotation obj' do
+      it "parse as entity types and apply id to denotation obj" do
         is_expected.to eq(expected_format)
       end
     end
 
-    context 'when source has metacharacter escape' do
+    context "when source has metacharacter escape" do
       let(:source) { '\[Elon Musk][Person] is a member of the [PayPal Mafia][Organization].' }
-      let(:expected_format) { {
-        "text": "[Elon Musk][Person] is a member of the PayPal Mafia.",
-        "denotations":[
-            {"span":{"begin": 40, "end": 52}, "obj":"Organization"}
-          ]
-      } }
-
-      it 'is not parsed as annotation' do
-        is_expected.to eq(expected_format)
-      end
-    end
-
-    context 'when reference definitions do not have a blank line above' do
-      let(:source) do
-        <<~MD2
-          [Elon Musk][Person] is a member of the PayPal Mafia.
-          [Person]: https://example.com/Person
-        MD2
-      end
-      let(:expected_format) { {
-        "text": "Elon Musk is a member of the PayPal Mafia.\n[Person]: https://example.com/Person",
-        "denotations":[
-            {"span":{"begin": 0, "end": 9}, "obj":"Person"}
-          ]
-      } }
-
-      it 'does not use as references' do
-        is_expected.to eq(expected_format)
-      end
-    end
-
-    context 'when reference definitions have a blank line above' do
-      let(:source) do
-        <<~MD2
-          [Elon Musk][Person] is a member of the PayPal Mafia.
-
-          [Person]: https://example.com/Person
-        MD2
-      end
-      let(:expected_format) { {
-        "text": "Elon Musk is a member of the PayPal Mafia.",
-        "denotations":[
-            {"span":{"begin": 0, "end": 9}, "obj":"https://example.com/Person"}
-          ],
-        "config": {
-          "entity types": [
-            { "id": "https://example.com/Person", "label": "Person" }
+      let(:expected_format) do
+        {
+          "text": "[Elon Musk][Person] is a member of the PayPal Mafia.",
+          "denotations": [
+            { "span": { "begin": 40, "end": 52 }, "obj": "Organization" }
           ]
         }
-      } }
+      end
 
-      it 'use definitions as references' do
+      it "is not parsed as annotation" do
         is_expected.to eq(expected_format)
       end
     end
 
-    context 'when reference contains additional text after url' do
-      context 'when text is enclosed with quotation' do
+    context "when reference definitions do not have a blank line above" do
+      let(:source) do
+        <<~MD2
+          [Elon Musk][Person] is a member of the PayPal Mafia.
+          [Person]: https://example.com/Person
+        MD2
+      end
+      let(:expected_format) do
+        {
+          "text": "Elon Musk is a member of the PayPal Mafia.\n[Person]: https://example.com/Person",
+          "denotations": [
+            { "span": { "begin": 0, "end": 9 }, "obj": "Person" }
+          ]
+        }
+      end
+
+      it "does not use as references" do
+        is_expected.to eq(expected_format)
+      end
+    end
+
+    context "when reference definitions have a blank line above" do
+      let(:source) do
+        <<~MD2
+          [Elon Musk][Person] is a member of the PayPal Mafia.
+
+          [Person]: https://example.com/Person
+        MD2
+      end
+      let(:expected_format) do
+        {
+          "text": "Elon Musk is a member of the PayPal Mafia.",
+          "denotations": [
+            { "span": { "begin": 0, "end": 9 }, "obj": "https://example.com/Person" }
+          ],
+          "config": {
+            "entity types": [
+              { "id": "https://example.com/Person", "label": "Person" }
+            ]
+          }
+        }
+      end
+
+      it "use definitions as references" do
+        is_expected.to eq(expected_format)
+      end
+    end
+
+    context "when reference contains additional text after url" do
+      context "when text is enclosed with quotation" do
         let(:source) do
           <<~MD2
             [Elon Musk][Person] is a member of the [PayPal Mafia][Organization].
@@ -115,26 +127,28 @@ RSpec.describe SimpleInlineTextAnnotation::Parser, type: :model do
             [Organization]: https://example.com/Organization 'text'
           MD2
         end
-        let(:expected_format) { {
-          "text": "Elon Musk is a member of the PayPal Mafia.",
-          "denotations":[
-              {"span":{"begin": 0, "end": 9}, "obj":"https://example.com/Person"},
-              {"span":{"begin": 29, "end": 41}, "obj":"https://example.com/Organization"},
+        let(:expected_format) do
+          {
+            "text": "Elon Musk is a member of the PayPal Mafia.",
+            "denotations": [
+              { "span": { "begin": 0, "end": 9 }, "obj": "https://example.com/Person" },
+              { "span": { "begin": 29, "end": 41 }, "obj": "https://example.com/Organization" }
             ],
-          "config": {
-            "entity types": [
-              { "id": "https://example.com/Person", "label": "Person" },
-              { "id": "https://example.com/Organization", "label": "Organization" }
-            ]
+            "config": {
+              "entity types": [
+                { "id": "https://example.com/Person", "label": "Person" },
+                { "id": "https://example.com/Organization", "label": "Organization" }
+              ]
+            }
           }
-        } }
+        end
 
-        it 'parsed as reference link' do
+        it "parsed as reference link" do
           is_expected.to eq(expected_format)
         end
       end
 
-      context 'when text is not enclosed with quotation' do
+      context "when text is not enclosed with quotation" do
         let(:source) do
           <<~MD2
             [Elon Musk][Person] is a member of the PayPal Mafia.
@@ -142,20 +156,22 @@ RSpec.describe SimpleInlineTextAnnotation::Parser, type: :model do
             [Person]: https://example.com/Person text
           MD2
         end
-        let(:expected_format) { {
-          "text": "Elon Musk is a member of the PayPal Mafia.\n\n[Person]: https://example.com/Person text",
-          "denotations":[
-              {"span":{"begin": 0, "end": 9}, "obj":"Person"}
+        let(:expected_format) do
+          {
+            "text": "Elon Musk is a member of the PayPal Mafia.\n\n[Person]: https://example.com/Person text",
+            "denotations": [
+              { "span": { "begin": 0, "end": 9 }, "obj": "Person" }
             ]
-        } }
+          }
+        end
 
-        it 'does not parse as reference link' do
+        it "does not parse as reference link" do
           is_expected.to eq(expected_format)
         end
       end
     end
 
-    context 'when text is written below the reference definition' do
+    context "when text is written below the reference definition" do
       let(:source) do
         <<~MD2
           [Elon Musk][Person] is a member of the PayPal Mafia.
@@ -164,24 +180,26 @@ RSpec.describe SimpleInlineTextAnnotation::Parser, type: :model do
           hello
         MD2
       end
-      let(:expected_format) { {
-        "text": "Elon Musk is a member of the PayPal Mafia.\n\nhello",
-        "denotations":[
-            {"span":{"begin": 0, "end": 9}, "obj":"https://example.com/Person"}
+      let(:expected_format) do
+        {
+          "text": "Elon Musk is a member of the PayPal Mafia.\n\nhello",
+          "denotations": [
+            { "span": { "begin": 0, "end": 9 }, "obj": "https://example.com/Person" }
           ],
-        "config": {
-          "entity types": [
-            { "id": "https://example.com/Person", "label": "Person" }
-          ]
+          "config": {
+            "entity types": [
+              { "id": "https://example.com/Person", "label": "Person" }
+            ]
+          }
         }
-      } }
+      end
 
-      it 'parse as expected format' do
+      it "parse as expected format" do
         is_expected.to eq(expected_format)
       end
     end
 
-    context 'when reference id is duplicated' do
+    context "when reference id is duplicated" do
       let(:source) do
         <<~MD2
           [Elon Musk][Person] is a member of the PayPal Mafia.
@@ -190,65 +208,71 @@ RSpec.describe SimpleInlineTextAnnotation::Parser, type: :model do
           [Person]: https://example.com/Organization
         MD2
       end
-      let(:expected_format) { {
-        "text": "Elon Musk is a member of the PayPal Mafia.",
-        "denotations":[
-            {"span":{"begin": 0, "end": 9}, "obj":"https://example.com/Person"},
+      let(:expected_format) do
+        {
+          "text": "Elon Musk is a member of the PayPal Mafia.",
+          "denotations": [
+            { "span": { "begin": 0, "end": 9 }, "obj": "https://example.com/Person" }
           ],
-        "config": {
-          "entity types": [
-            { "id": "https://example.com/Person", "label": "Person" }
-          ]
+          "config": {
+            "entity types": [
+              { "id": "https://example.com/Person", "label": "Person" }
+            ]
+          }
         }
-      } }
+      end
 
-      it 'use first defined id in priority' do
+      it "use first defined id in priority" do
         is_expected.to eq(expected_format)
       end
     end
 
-    context 'when reference label and id is same' do
+    context "when reference label and id is same" do
       let(:source) do
         <<~MD2
           [Elon Musk][Person] is a member of the PayPal Mafia.
 
           [Person]: Person
-          MD2
+        MD2
       end
-      let(:expected_format) { {
-        "text": "Elon Musk is a member of the PayPal Mafia.",
-        "denotations":[
-            {"span":{"begin": 0, "end": 9}, "obj":"Person"},
-          ],
-      } }
+      let(:expected_format) do
+        {
+          "text": "Elon Musk is a member of the PayPal Mafia.",
+          "denotations": [
+            { "span": { "begin": 0, "end": 9 }, "obj": "Person" }
+          ]
+        }
+      end
 
-      it 'is do not create config' do
+      it "is do not create config" do
         is_expected.to eq(expected_format)
       end
     end
 
-    context 'when consecutive newlines in source' do
+    context "when consecutive newlines in source" do
       let(:source) do
         <<~MD2
           [Elon Musk][Person] is a member of the PayPal Mafia.
 
 
           Elon Musk is a member of the PayPal Mafia.
-          MD2
+        MD2
       end
-      let(:expected_format) { {
-        "text": "Elon Musk is a member of the PayPal Mafia.\n\nElon Musk is a member of the PayPal Mafia.",
-        "denotations":[
-            {"span":{"begin": 0, "end": 9}, "obj":"Person"},
+      let(:expected_format) do
+        {
+          "text": "Elon Musk is a member of the PayPal Mafia.\n\nElon Musk is a member of the PayPal Mafia.",
+          "denotations": [
+            { "span": { "begin": 0, "end": 9 }, "obj": "Person" }
           ]
-      } }
+        }
+      end
 
-      it 'is parsed as single newline' do
+      it "is parsed as single newline" do
         is_expected.to eq(expected_format)
       end
     end
 
-    context 'when white spaces before reference definition' do
+    context "when white spaces before reference definition" do
       let(:source) do
         # Using <<- to create white spaces
         <<-MD2
@@ -258,21 +282,23 @@ RSpec.describe SimpleInlineTextAnnotation::Parser, type: :model do
           [Organization]: https://example.com/Organization
         MD2
       end
-      let(:expected_format) { {
-        "text": "Elon Musk is a member of the PayPal Mafia.",
-        "denotations":[
-            {"span":{"begin": 0, "end": 9}, "obj":"https://example.com/Person"},
-            {"span":{"begin": 29, "end": 41}, "obj":"https://example.com/Organization"},
+      let(:expected_format) do
+        {
+          "text": "Elon Musk is a member of the PayPal Mafia.",
+          "denotations": [
+            { "span": { "begin": 0, "end": 9 }, "obj": "https://example.com/Person" },
+            { "span": { "begin": 29, "end": 41 }, "obj": "https://example.com/Organization" }
           ],
-        "config": {
-          "entity types": [
-            { "id": "https://example.com/Person", "label": "Person" },
-            { "id": "https://example.com/Organization", "label": "Organization" }
-          ]
+          "config": {
+            "entity types": [
+              { "id": "https://example.com/Person", "label": "Person" },
+              { "id": "https://example.com/Organization", "label": "Organization" }
+            ]
+          }
         }
-      } }
+      end
 
-      it 'parse as entity types with ignoring white spaces' do
+      it "parse as entity types with ignoring white spaces" do
         is_expected.to eq(expected_format)
       end
     end
