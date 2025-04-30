2f8e716375144a38ee5b074f95773ddc5de19cf4
xaiBUh29wX <arino.tamada@luxiar.com>
2025-04-30 13:35:35 +0900

    Restore previous test cases in a new file with updated naming

--- js/__tests__/parser.denotation.test.mjs
+++ js/__tests__/parser.denotation.test.mjs
@@ -35,82 +35,37 @@ describe('SimpleInlineTextAnnotation.parse', () => {
     expect(SimpleInlineTextAnnotation.parse(source)).toStrictEqual(expected);
   });
 
-  test('should parse as denotation when source has annotation structure', () => {
-    const source = "[Elon Musk][T1, Person, member_of, T2] is a member of the [PayPal Mafia][T2, Organization].";
-    const expected = {
-      text: "Elon Musk is a member of the PayPal Mafia.",
-      denotations: [
-        { id: "T1", span: { begin: 0, end: 9 }, obj: "Person" },
-        { id: "T2", span: { begin: 29, end: 41 }, obj: "Organization" }
-      ],
-      relations: [
-        { subj: "T1", pred: "member_of", obj: "T2" }
-      ]
-    };
-    expect(SimpleInlineTextAnnotation.parse(source)).toStrictEqual(expected);
-  });
-
-  test('should parse as entity types and apply id to denotation obj when source has reference structure', () => {
-    const source = `[Elon Musk][T1, Person, member_of, T2] is a member of the [PayPal Mafia][T2, Organization].
-
-[Person]: https://example.com/Person
-[Organization]: https://example.com/Organization`;
-    const expected = {
-      text: "Elon Musk is a member of the PayPal Mafia.",
-      denotations: [
-        { id: "T1", span: { begin: 0, end: 9 }, obj: "https://example.com/Person" },
-        { id: "T2", span: { begin: 29, end: 41 }, obj: "https://example.com/Organization" }
-      ],
-      relations: [
-        { subj: "T1", pred: "member_of", obj: "T2" }
-      ],
-      config: {
-        "entity types": [
-          { id: "https://example.com/Person", label: "Person" },
-          { id: "https://example.com/Organization", label: "Organization" }
-        ]
-      }
-    };
-    expect(SimpleInlineTextAnnotation.parse(source)).toStrictEqual(expected);
-  });
-
   test('should not parse as annotation when source has metacharacter escape', () => {
-    const source = '\\[Elon Musk][T1, Person, member_of, T2] is a member of the [PayPal Mafia][T2, Organization].';
+    const source = '\\[Elon Musk][Person] is a member of the [PayPal Mafia][Organization].';
     const expected = {
-      text: "[Elon Musk][T1, Person, member_of, T2] is a member of the PayPal Mafia.",
+      text: "[Elon Musk][Person] is a member of the PayPal Mafia.",
       denotations: [
-        { id: "T2", span: { begin: 59, end: 71 }, obj: "Organization" }
+        { span: { begin: 40, end: 52 }, obj: "Organization" }
       ]
     };
     expect(SimpleInlineTextAnnotation.parse(source)).toStrictEqual(expected);
   });
 
   test('should not use as references when reference definitions do not have a blank line above', () => {
-    const source = `[Elon Musk][T1, Person, member_of, T2] is a member of the PayPal Mafia.
+    const source = `[Elon Musk][Person] is a member of the PayPal Mafia.
 [Person]: https://example.com/Person`;
     const expected = {
       text: "Elon Musk is a member of the PayPal Mafia.\n[Person]: https://example.com/Person",
       denotations: [
-        { id: "T1", span: { begin: 0, end: 9 }, obj: "Person" }
-      ],
-      relations: [
-        { subj: "T1", pred: "member_of", obj: "T2" }
+        { span: { begin: 0, end: 9 }, obj: "Person" }
       ]
     };
     expect(SimpleInlineTextAnnotation.parse(source)).toStrictEqual(expected);
   });
 
   test('should use definitions as references when reference definitions have a blank line above', () => {
-    const source = `[Elon Musk][T1, Person, member_of, T2] is a member of the PayPal Mafia.
+    const source = `[Elon Musk][Person] is a member of the PayPal Mafia.
 
 [Person]: https://example.com/Person`;
     const expected = {
       text: "Elon Musk is a member of the PayPal Mafia.",
       denotations: [
-        { id: "T1", span: { begin: 0, end: 9 }, obj: "https://example.com/Person" }
-      ],
-      relations: [
-        { subj: "T1", pred: "member_of", obj: "T2" }
+        { span: { begin: 0, end: 9 }, obj: "https://example.com/Person" }
       ],
       config: {
         "entity types": [
@@ -122,18 +77,15 @@ describe('SimpleInlineTextAnnotation.parse', () => {
   });
 
   test('should parse as reference link when reference contains additional text enclosed with quotation', () => {
-    const source = `[Elon Musk][T1, Person, member_of, T2] is a member of the [PayPal Mafia][T2, Organization].
+    const source = `[Elon Musk][Person] is a member of the [PayPal Mafia][Organization].
 
 [Person]: https://example.com/Person "text"
 [Organization]: https://example.com/Organization 'text'`;
     const expected = {
       text: "Elon Musk is a member of the PayPal Mafia.",
       denotations: [
-        { id: "T1", span: { begin: 0, end: 9 }, obj: "https://example.com/Person" },
-        { id: "T2", span: { begin: 29, end: 41 }, obj: "https://example.com/Organization" }
-      ],
-      relations: [
-        { subj: "T1", pred: "member_of", obj: "T2" }
+        { span: { begin: 0, end: 9 }, obj: "https://example.com/Person" },
+        { span: { begin: 29, end: 41 }, obj: "https://example.com/Organization" }
       ],
       config: {
         "entity types": [
@@ -146,33 +98,27 @@ describe('SimpleInlineTextAnnotation.parse', () => {
   });
 
   test('should not parse as reference link when reference contains additional text not enclosed with quotation', () => {
-    const source = `[Elon Musk][T1, Person, member_of, T2] is a member of the PayPal Mafia.
+    const source = `[Elon Musk][Person] is a member of the PayPal Mafia.
 
 [Person]: https://example.com/Person text`;
     const expected = {
       text: "Elon Musk is a member of the PayPal Mafia.\n\n[Person]: https://example.com/Person text",
       denotations: [
-        { id: "T1", span: { begin: 0, end: 9 }, obj: "Person" }
-      ],
-      relations: [
-        { subj: "T1", pred: "member_of", obj: "T2" }
+        { span: { begin: 0, end: 9 }, obj: "Person" }
       ]
     };
     expect(SimpleInlineTextAnnotation.parse(source)).toStrictEqual(expected);
   });
 
   test('should parse as expected format when text is written below the reference definition', () => {
-    const source = `[Elon Musk][T1, Person, member_of, T2] is a member of the PayPal Mafia.
+    const source = `[Elon Musk][Person] is a member of the PayPal Mafia.
 
 [Person]: https://example.com/Person
 hello`;
     const expected = {
       text: "Elon Musk is a member of the PayPal Mafia.\n\nhello",
       denotations: [
-        { id: "T1", span: { begin: 0, end: 9 }, obj: "https://example.com/Person" }
-      ],
-      relations: [
-        { subj: "T1", pred: "member_of", obj: "T2" }
+        { span: { begin: 0, end: 9 }, obj: "https://example.com/Person" }
       ],
       config: {
         "entity types": [
@@ -184,17 +130,14 @@ hello`;
   });
 
   test('should use first defined id in priority when reference id is duplicated', () => {
-    const source = `[Elon Musk][T1, Person, member_of, T2] is a member of the PayPal Mafia.
+    const source = `[Elon Musk][Person] is a member of the PayPal Mafia.
 
 [Person]: https://example.com/Person
 [Person]: https://example.com/Organization`;
     const expected = {
       text: "Elon Musk is a member of the PayPal Mafia.",
       denotations: [
-        { id: "T1", span: { begin: 0, end: 9 }, obj: "https://example.com/Person" }
-      ],
-      relations: [
-        { subj: "T1", pred: "member_of", obj: "T2" }
+        { span: { begin: 0, end: 9 }, obj: "https://example.com/Person" }
       ],
       config: {
         "entity types": [
@@ -206,51 +149,42 @@ hello`;
   });
 
   test('should not create config when reference label and id are the same', () => {
-    const source = `[Elon Musk][T1, Person, member_of, T2] is a member of the PayPal Mafia.
+    const source = `[Elon Musk][Person] is a member of the PayPal Mafia.
 
 [Person]: Person`;
     const expected = {
       text: "Elon Musk is a member of the PayPal Mafia.",
       denotations: [
-        { id: "T1", span: { begin: 0, end: 9 }, obj: "Person" }
-      ],
-      relations: [
-        { subj: "T1", pred: "member_of", obj: "T2" }
+        { span: { begin: 0, end: 9 }, obj: "Person" }
       ]
     };
     expect(SimpleInlineTextAnnotation.parse(source)).toStrictEqual(expected);
   });
 
   test('should parse as single newline when consecutive newlines in source', () => {
-    const source = `[Elon Musk][T1, Person, member_of, T2] is a member of the PayPal Mafia.
+    const source = `[Elon Musk][Person] is a member of the PayPal Mafia.
 
 
 Elon Musk is a member of the PayPal Mafia.`;
     const expected = {
       text: "Elon Musk is a member of the PayPal Mafia.\n\nElon Musk is a member of the PayPal Mafia.",
       denotations: [
-        { id: "T1", span: { begin: 0, end: 9 }, obj: "Person" }
-      ],
-      relations: [
-        { subj: "T1", pred: "member_of", obj: "T2" }
+        { span: { begin: 0, end: 9 }, obj: "Person" }
       ]
     };
     expect(SimpleInlineTextAnnotation.parse(source)).toStrictEqual(expected);
   });
 
   test('should parse as entity types with ignoring white spaces before reference definition', () => {
-    const source = `  [Elon Musk][T1, Person, member_of, T2] is a member of the [PayPal Mafia][T2, Organization].
+    const source = `  [Elon Musk][Person] is a member of the [PayPal Mafia][Organization].
 
   [Person]: https://example.com/Person
   [Organization]: https://example.com/Organization`;
     const expected = {
       text: "Elon Musk is a member of the PayPal Mafia.",
       denotations: [
-        { id: "T1", span: { begin: 0, end: 9 }, obj: "https://example.com/Person" },
-        { id: "T2", span: { begin: 29, end: 41 }, obj: "https://example.com/Organization" }
-      ],
-      relations: [
-        { subj: "T1", pred: "member_of", obj: "T2" }
+        { span: { begin: 0, end: 9 }, obj: "https://example.com/Person" },
+        { span: { begin: 29, end: 41 }, obj: "https://example.com/Organization" }
       ],
       config: {
         "entity types": [
@@ -260,40 +194,6 @@ Elon Musk is a member of the PayPal Mafia.`;
       }
     };
     expect(SimpleInlineTextAnnotation.parse(source)).toStrictEqual(expected);
-  });
-
-  test("should not convert brackets when the number of annotations in brackets is invalid", () => {
-    const source = "[Elon Musk][T1, Person, member_of, T2, Hoge] is a member of the [PayPal Mafia][T2, Organization, Fuga].";
-    const expected = {
-      text: "[Elon Musk][T1, Person, member_of, T2, Hoge] is a member of the [PayPal Mafia][T2, Organization, Fuga].",
-      denotations: []
-    };
-    expect(SimpleInlineTextAnnotation.parse(source)).toStrictEqual(expected);
-  });
-
-  describe('when there are three consecutive brackets', () => {
-    it('should process valid annotations and ignore the third bracket', () => {
-      const source = "[Elon Musk][T1, Person, member_of, T2][Piyo] is a member of the PayPal Mafia.";
-      const expected = {
-        text: "Elon Musk[Piyo] is a member of the PayPal Mafia.",
-        denotations: [
-          { id: "T1", span: { begin: 0, end: 9 }, obj: "Person" }
-        ],
-        relations: [
-          { pred: "member_of", subj: "T1", obj: "T2" }
-        ]
-      };
-      expect(SimpleInlineTextAnnotation.parse(source)).toStrictEqual(expected);
-    });
-
-    it('should skip invalid annotations and ignore the third bracket', () => {
-      const source = "[Elon Musk][T1, Person, member_of, T2, Hoge][Piyo] is a member of the PayPal Mafia.";
-      const expected = {
-        text: "[Elon Musk][T1, Person, member_of, T2, Hoge][Piyo] is a member of the PayPal Mafia.",
-        denotations: []
-      };
-      expect(SimpleInlineTextAnnotation.parse(source)).toStrictEqual(expected);
-    });
   });
 });
 
